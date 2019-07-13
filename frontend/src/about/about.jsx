5e244f9344ddb586ee934c12532f3f38e2818f6d
kapeles <kapeles@debian-PC>
2019-07-13 15:23:07 -0300

    Adicionado pageheader nos modulos de paginação

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='nós' />
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum da malásia</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum da malásia</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum da malásia</p>
+    <h2>Contato</h2>
+    <p>Lorem ipsum da malásia</p>
   </div>
 )
 
