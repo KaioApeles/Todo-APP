a3586c20e383d4534fe8e8d1229105e0faaea80c
kapeles <kapeles@debian-PC>
2019-07-14 12:44:32 -0300

    app sem redux

---
+++ Todo-sem-redux/frontend/src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='nós' />
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum da malásia</p>
+    <h2>Missão e Visão</h2>
+    <p>Lorem ipsum da malásia</p>
+    <h2>Imprensa</h2>
+    <p>Lorem ipsum da malásia</p>
+    <h2>Contato</h2>
+    <p>Lorem ipsum da malásia</p>
+  </div>
+)
+
